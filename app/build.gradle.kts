83053c905009843f5457b79cb6cf6543a38f0c14
Debjyoti Gorai <debjyotigorai@outlook.com>
2026-09-23 16:26:04 +0530

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,5 +41,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.zxing:core:3.5.3")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
